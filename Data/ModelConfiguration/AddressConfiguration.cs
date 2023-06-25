b598ee45fbc3e76255b8e719d2a8705a5272f5f9
Metin Çelebi <metincelebi5534@gmail.com>
2023-06-25 21:27:04 +0300

    final projects **İhtiyaçlar Kategori ekleme güncelleme apileri. Listeleme apileri. Kategori bazlı urun listeleme apisi . Kategori silme apisi (Kategoride ürün varsa silinemez) Ürün ekleme güncelleme apisi. Listeleme apisi. Silme apisi Kullanıcı oluşturma apisi. Login apisi. Kullanıcı güncelleme ve silme apisi. Yetkilendirme icin jwt token altyapısı Sipariş apileri, oluşturma , aktif siparişler, geçmiş sipariş apileri Sipariş detay apisi, siparişteki ürün bilgileri Kupon oluşturma listeleme ve silme apileri Puan sorgulama apisi **Tech-Stack Veri tabanı (Postgresql,Mssql) JWT token (Yetkilendirme) EF-Repositroy - Unitofwork Postman Redis (Basket controller)

---
+++ Data/ModelConfiguration/AddressConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Data.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.ModelConfiguration
+{
+    public class AddressConfiguration : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.ToTable(name: "Address", schema: "dbo");
+            builder.Property(x => x.Province).IsRequired(true).HasMaxLength(100);
+            builder.Property(x => x.District).IsRequired(true).HasMaxLength(100);
+            builder.Property(x => x.Street).IsRequired(true).HasMaxLength(100);
+            builder.Property(x => x.ZipCode).IsRequired(true).HasMaxLength(100);
+            builder.Property(x => x.Line).IsRequired(true).HasMaxLength(100);
+        }
+    }
+}
